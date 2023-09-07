cc41dde22e248b9bbef0c8e76fbb3b1a147ab3fd
가은 이 <lge0325@bme.gachon.ac.kr>
2023-09-07 17:33:27 +0900

    ➕[Feat] Add 기능

--- LibContactBook/Add.c
+++ LibContactBook/Add.c
@@ -1,1 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include "pch.h"
+#include <stdio.h>
+#include <malloc.h>
+
+typedef struct _USERDATA
+{
+	char szName[32];
+	char szPhone[32];
+	int category;
+	struct _USERDATA* pNext;
+} USERDATA;
+
+void Add() {
+	USERDATA UserData = { 0 };
+	gets(UserData.szName);
+	puts(UserData.szName);
+}
