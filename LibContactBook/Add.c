721b936390238fb916bb6ffd801a4508c9f0d636
nasha430@daum.net <nasha430@daum.net>
2023-09-07 17:30:31 +0900

    ➕[Feat] : 기능 추가 Add() 구조체 작성중

--- LibContactBook/Add.c
+++ LibContactBook/Add.c
@@ -1,1 +1,21 @@
 #include "pch.h"
+#include <stdio.h>
+struct USERDATA 
+{
+	char szName[32];
+	char szPhone[32];
+	int category;
+	struct _USERDATA* pNext;
+};
+
+void Add() {
+	struct USERDATA USERDATA = {
+		"±è°æ¼ö",
+		"010-7554-9154",
+		0,
+		NULL
+	};
+	struct USERDATA *name = &USERDATA;
+	
+	printf("%s", name->szName);
+}
--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 ﻿#include <stdio.h>
 
 void main() {
-
+	
+	Add();
 }
