01ccd01ae7bc8481091f025718e1dc73581d4a37
가은 이 <lge0325@bme.gachon.ac.kr>
2023-09-07 17:39:24 +0900

    🚀[Chore] 원상복귀

--- main.c
+++ main.c
@@ -1,27 +1,5 @@
-﻿#include "pch.h"
-#include <stdio.h>
-#include <malloc.h>
-
-typedef struct _USERDATA
-{
-	char szName[32];
-	char szPhone[32];
-	int category;
-	struct _USERDATA* pNext;
-} USERDATA;
+﻿#include <stdio.h>
 
 void main() {
 
-
-	char menu = (char*)malloc(sizeof(char) + 1); // size 4 동적할당
-	char temp = "";
-
-	while (temp != 'X') {
-		temp = MainMenu(menu);
-		
-		if (temp == 'A') {
-			printf("hi");
-			Add();
-		}
-	}
 }
