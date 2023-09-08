62862bb0a380c81b8af64547d258a401774e380b
가은 이 <lge0325@bme.gachon.ac.kr>
2023-09-08 17:45:34 +0900

    🚨[Alert] printAll 중복 호출 시 실행 안됨

--- LibContactBook/MainMenu.c
+++ LibContactBook/MainMenu.c
@@ -3,12 +3,12 @@
 #include <stdio.h>
 #include <malloc.h>
 
-char MainMenu(menu) {
+char MainMenu(char* menu) {
 	printf("\n");
 	printf("--------------------------------전화번호부 인터페이스--------------------------------\n");
 	printf("추가[A]  검색[S]  전체 출력[P]  삭제[R]  수정[U]  카테고리 출력[C]  프로그램 종료[X]\n");
 
-	char menu_tmp;
+	char* menu_tmp;
 	printf("원하시는 명령어를 입력하세요 : ");
 	// 궁금 - 한줄로 못받나?
 	scanf_s("%c", &menu_tmp);
