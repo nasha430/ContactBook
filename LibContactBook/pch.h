486a0d5c86e754204b455830a9f272a5975b8124
가은 이 <lge0325@bme.gachon.ac.kr>
2023-09-11 17:50:14 +0900

    🚧[Refactor] 라이브러리

--- LibContactBook/pch.h
+++ LibContactBook/pch.h
@@ -1,9 +1,21 @@
 ﻿#pragma once
+
+typedef struct USERDATA
+{
+	char szName[32];
+	char szPhone[32];
+	int category;
+	struct USERDATA* pNext;
+} USERDATA;
+
 void MainMenu(menu_tmp);
-void Add();
+USERDATA* Create();
+void Add(USERDATA** Head, USERDATA* NewNode);
 void Search();
 void PrintAll();
 void Remove();
 void Update();
 void Category();
+void UI();
 void Exit();
+
