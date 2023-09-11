ee2edadfc15e5c51c4ecae1382dcff56bfee22c4
nasha430@daum.net <nasha430@daum.net>
2023-09-11 10:57:36 +0900

    🚧[Refactor] add() 포인터 프린트 수정.

--- LibContactBook/Add.c
+++ LibContactBook/Add.c
@@ -2,37 +2,44 @@
 #include "pch.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <malloc.h>
-typedef struct _USERDATA
+typedef struct USERDATA
 {
 	char szName[32];
 	char szPhone[32];
 	int category;
-	struct _USERDATA* pNext;
-}USERDATA;
+	struct USERDATA* pNext;
+} USERDATA;
+
 void Add(char* UserData) {
+
 	USERDATA* ContactBook;
-	// 현재(n) 포인터
+	// 동적할당 받은 주소를 구조체 변수에 할당하기
 	ContactBook = UserData;
-	// 임시적으로 저장할 (n-1)포인터
+
+	// 새로운 값의 주소를 임시 저장
 	static USERDATA* pTmp;
-	//printf("UserData 포인터: %p, pNext의 포인터 :%p", &UserData, ContactBook.pNext);
+	USERDATA* prevContactBook = NULL;
+
 	printf("추가할 이름을 입력하세요:");
 	scanf("%s", ContactBook->szName);
 	printf("이름에 맞는 전화번호를 입력하세요(010-0000-0000):");
 	scanf("%s", ContactBook->szPhone);
 	printf("카테고리에 맞는 숫자를 입력하세요. 친구[0], 가족[1], 직장[2], 기타[3]:");
 	scanf("%d", &ContactBook->category);
-
+	getchar();
+	// 완전 처음에!
 	if (pTmp == NULL) {
+		// 현재 구조체는 NULL , 다음 주소에 넘길 주소를 pTmp 에 저장
 		pTmp = UserData;
-		ContactBook->pNext;
+		ContactBook->pNext = NULL;
 	}
 	else {
-		USERDATA* preventContactBook;
-		preventContactBook = pTmp;
+		// 이전 구조체 주소에 현재 주소 넣기
 
-		preventContactBook->pNext=UserData;
+		prevContactBook = pTmp;
+		prevContactBook->pNext = UserData;
+
+		// 현재 구조체는 NULL , 다음 주소에 넘길 주소를 pTmp 에 저장
 		pTmp = UserData;
 		ContactBook->pNext = NULL;
 	}
@@ -41,6 +48,6 @@ void Add(char* UserData) {
 	FILE* pfile = NULL;
 	pfile = fopen("Contactbook.txt", "a");
 	fprintf(pfile, "\n");
-	fprintf(pfile, "이름: %s, 전화번호: %s, 카테고리: %d, 현재 pNext포인터%p", ContactBook->szName, ContactBook->szPhone, ContactBook->category, UserData);
+	fprintf(pfile, "이름: %s, 전화번호: %s, 카테고리: %d:, 현재 구조체 포인터: %p, 이전 구조체 포인터: %p, 최종 노드 구조체 포인터: %p", ContactBook->szName, ContactBook->szPhone, ContactBook->category, prevContactBook->pNext, prevContactBook, ContactBook->pNext);
 	fclose(pfile);
 }
