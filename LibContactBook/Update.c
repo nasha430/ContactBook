b1ee7748b657b02b48bc339592e6f0076d772b3a
가은 이 <lge0325@bme.gachon.ac.kr>
2023-09-15 17:17:02 +0900

    ➕[Feat] Update 기능 완성 (이름 넣어서 전번, 카테고리 수정)

--- LibContactBook/Update.c
+++ LibContactBook/Update.c
@@ -1,1 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include "pch.h"
+#include <stdio.h>
+#include <malloc.h>
+#include <string.h>
+
+void Update(USERDATA* Head) {
+    UpdateUI();
+
+    USERDATA* Current = Head;
+
+    char UpdateName[32];
+    char UpdateNameTemp[32];
+    char UpdatePhone[32];
+    int UpdateCategory = 0;
+    scanf("%s", UpdateName);
+
+    while (Current->pNext != NULL)
+    {
+        //구조체 이름과 수정할 이름이 같으면 출력한다.
+        strcpy(UpdateNameTemp, Current->szName);
+        if (*UpdateName == *UpdateNameTemp) {
+            printf("수정하고 싶은 전화번호를 입력하세요 : ");
+            scanf("%s", &UpdatePhone);
+            strcpy(Current->szPhone, UpdatePhone);
+            printf("수정하고 싶은 카테고리를 입력하세요 친구[0], 가족[1], 직장[2], 기타[3] : ");
+            scanf("%d", &UpdateCategory);
+            getchar();
+            Current->category = UpdateCategory;
+            break;
+        }
+        Current = Current->pNext;
+    }
+}
