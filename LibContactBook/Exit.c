c7f1cc0cf7e7d02ceed96e22727e2a046dbf4265
gani0325 <lge0325@bme.gachon.ac.kr>
2023-09-17 22:34:41 +0900

    ➕[Feat] 동적할당 메모리 소멸시키기

--- LibContactBook/Exit.c
+++ LibContactBook/Exit.c
@@ -1,1 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include "pch.h"
+#include <stdio.h>
+#include <malloc.h>
+#include <string.h>
+
+//  ³ëµå ¼Ò¸ê 
+void Exit(USERDATA* Head)
+{
+    USERDATA* Current = Head;
+
+    while (Current->pNext != NULL) {
+        free(Current);
+    }
+    
+}
